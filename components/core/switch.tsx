c0870feebc175f9a4b61e98aaf772a2570b952e3
Kyle Scott <kyscott@umich.edu>
2023-04-28 12:24:16 -0400

    switch styling

--- components/core/switch.tsx
+++ components/core/switch.tsx
@@ -10,15 +10,15 @@ export default function Switch() {
       checked={enabled}
       onChange={setEnabled}
       className={clsx(
-        "relative inline-flex h-6 w-11 items-center rounded-2xl",
+        "relative inline-flex h-7 w-11 items-center rounded-2xl",
         enabled ? "bg-[#3b82f6]" : "bg-gray-700"
       )}
     >
       <span className="sr-only">Enable notifications</span>
       <span
         className={clsx(
-          enabled ? "translate-x-6" : "translate-x-1",
-          "inline-block h-4 w-4 transform rounded-full bg-white transition"
+          enabled ? "translate-x-5" : "translate-x-1",
+          "inline-block h-5 w-5 transform rounded-full bg-white transition"
         )}
       />
     </HeadlessSwitch>
